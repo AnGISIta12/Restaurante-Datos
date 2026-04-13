9324be548f4f6e9d1daabaa8ec9effee557cf305
AnGISIta12 <angylorena0912@gmail.com>
2026-04-12 20:49:13 -0500

    conectado a base

--- etc/parametros.php
+++ etc/parametros.php
@@ -2,10 +2,6 @@
 $host = "localhost";
 $dbname = "Restaurante";
 $user = "postgres";
-<<<<<<< HEAD
-$password = "posabgress2025$$";
-=======
 $password = "la contra se le pide a Angisita";
->>>>>>> 98c63fad331687de1274af9ca24ce599aaf6c7c1
 
 ?>
